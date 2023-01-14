1b05196c96c124860dd7dc423d3b6ee0b7113d39
gabriel-d-guillermo <109959568+gabriel-d-guillermo@users.noreply.github.com>
2023-01-15 03:32:40 +0800

    initial commit

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,11 +2,14 @@ import { useState, useMemo, useEffect, useCallback, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
 import "../components/ProductCard.css";
-export default function ProductById() {
+
+export default function Product() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     async function getProducts() {
       try {
         const product = await fetch(`${process.env.REACT_APP_API_URL}/products/viewProducts`, {
@@ -15,26 +18,39 @@ export default function ProductById() {
         const productData = await product.json();
         console.log(productData);
         setProducts(productData);
+        setLoading(false);
       } catch (error) {
         if (error.name === "AbortError") {
-          console.log("wew");
           console.warn("Request was cancelled");
         } else {
           console.error(error);
+          setLoading(false);
         }
       }
     }
     getProducts();
     return () => controller.abort();
   }, []);
-
+  // if (loading) {
+  //   return (
+  //     <div className="spinner-border" style={{ width: "3rem", height: "3rem" }} role="status">
+  //       <span className="visually-hidden">Loading...</span>
+  //     </div>
+  //   );
+  // }
   return (
     <Container fluid="md" className="product ">
-      <Row className="">
-        {products.map((product, index) => {
-          return <ProductCard key={index} prop={product} />;
-        })}
-      </Row>
+      {loading ? (
+        <div className="spinner-border" style={{ width: "5rem", height: "5rem" }} role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <Row className="">
+          {products.map((product, index) => {
+            return <ProductCard key={index} prop={product} />;
+          })}
+        </Row>
+      )}
     </Container>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,33 +1,119 @@
 import { useState, useEffect, useCallback, useMemo, useRef } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { Form, Button, Card, Container } from "react-bootstrap";
-
+import Swal from "sweetalert2";
 export default function Register() {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formError, setFormError] = useState({});
   const [isFormValid, setIsFormValid] = useState(false);
 
-  const usernameRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
 
+  const handleReset = () => {
+    firstNameRef.current.value = "";
+    lastNameRef.current.value = "";
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    confirmPasswordRef.current.value = "";
+  };
+
   const handleForm = e => {
     e.preventDefault();
     let error = {};
-    const username = usernameRef.current.value;
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    const confirmPassword = confirmPasswordRef.current.value;
-    if (username.trim() === "") error.username = "username is required";
-    if (email.trim() === "") error.email = "email is required";
-    if (password.trim() === "") error.password = "password is required";
-    if (password.trim() !== confirmPassword.trim())
-      error.confirmPassword = "password and confirm password should be the same";
-    console.log(password === confirmPassword);
+    const firstName = firstNameRef.current?.value.trim();
+    const lastName = lastNameRef.current?.value.trim();
+    const email = emailRef.current?.value.trim();
+    const password = passwordRef.current?.value.trim();
+    const confirmPassword = confirmPasswordRef.current?.value.trim();
+    if (firstName === "") error.firstName = "first name is required";
+    if (lastName === "") error.lastName = "last name is required";
+    if (email === "") error.email = "email is required";
+    if (password === "") error.password = "password is required";
+    if (password !== confirmPassword) error.confirmPassword = "password and confirm password should be the same";
     setFormError(error);
+    if (Object.keys(error).length === 0) {
+      setIsFormValid(true);
+    }
   };
+
+  const checkEmail = async () => {
+    try {
+      const userEmail = await fetch(`${process.env.REACT_APP_API_URL}/users/checkEmail`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailRef.current.value,
+        }),
+      });
+
+      const data = await userEmail.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const register = async () => {
+      try {
+        const isEmailInUSe = await checkEmail();
+        if (isEmailInUSe) {
+          Swal.fire({
+            // title: "Duplicate Email Found",
+            icon: "warning",
+            text: "Email is already in use. Please use a different one",
+
+            width: "25rem",
+            // buttonsStyling: false,
+            // allowOutsideClick: true,
+          });
+
+          return;
+        }
+        // if (isFormValid) {
+        //   const data = await fetch(`${process.env.REACT_APP_API_URL}/users/register`, {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //       username: "",
+        //       email: "",
+        //       password: "",
+        //     }),
+        //   });
+
+        //   const res = await data.json();
+        //   console.log(res);
+        //   handleReset();
+        //   Swal.fire({
+        //     position: "center",
+        //     icon: "success",
+        //     title: "register success!!",
+        //     showConfirmButton: false,
+        //     timer: 2000,
+        //     toast: true,
+        //   });
+        // }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (isFormValid) register();
+
+    return () => {
+      setIsFormValid(false);
+    };
+  }, [isFormValid]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -35,31 +121,43 @@ export default function Register() {
     <>
       {/* <Alert variant="primary">This is a alert—check it out!</Alert> */}
       <Container fluid className=" background-container pt-2 ">
-        <Card className="register mx-auto my-3 border pt-3 border-dark" style={{ maxWidth: "30rem", fontSize: "14px" }}>
+        <Card className="register mx-auto mt-3 border pt-3 border-dark" style={{ maxWidth: "30rem", fontSize: "14px" }}>
           <Card.Body className="p-0">
             <Card.Title className="text-center fw-bold">Register</Card.Title>
 
             <Form className=" px-4 py-2 " onSubmit={handleForm}>
-              <Form.Group className="mb-3" controlId="UserName">
-                <Form.Label>User name</Form.Label>
+              <Form.Group className="mb-3" controlId="firstName">
+                <Form.Label className="mb-1">First name</Form.Label>
                 <Form.Control
                   size="sm"
-                  ref={usernameRef}
+                  ref={firstNameRef}
                   type="text"
-                  placeholder="Enter user name"
+                  placeholder="Enter first name"
                   autoComplete="off"
                 />
-                {formError.username && <span className="error">{formError.username}</span>}
+                {formError.firstName && <span className="error">{formError.firstName}</span>}
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="lastName">
+                <Form.Label className="mb-1">Last name</Form.Label>
+                <Form.Control
+                  size="sm"
+                  ref={lastNameRef}
+                  type="text"
+                  placeholder="Enter first name"
+                  autoComplete="off"
+                />
+                {formError.lastName && <span className="error">{formError.lastName}</span>}
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email address</Form.Label>
+                <Form.Label className="mb-1">Email address</Form.Label>
                 <Form.Control size="sm" ref={emailRef} type="email" placeholder="Enter email" autoComplete="off" />
                 {formError.email && <span className="error">{formError.email}</span>}
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
+                <Form.Label className="mb-1">Password</Form.Label>
                 <Form.Control
                   size="sm"
                   ref={passwordRef}
@@ -69,8 +167,8 @@ export default function Register() {
                 />
                 {formError.password && <span className="error">{formError.password}</span>}
               </Form.Group>
-              <Form.Group className="mb-3 " controlId="confirmPassword">
-                <Form.Label>Confirm Password</Form.Label>
+              <Form.Group className="mb-1 " controlId="confirmPassword">
+                <Form.Label className="mb-1">Confirm Password</Form.Label>
                 <Form.Control
                   size="sm"
                   ref={confirmPasswordRef}
