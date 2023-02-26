391a542f6facca623b38353bab833435d5460ee4
gabriel-d-guillermo <109959568+gabriel-d-guillermo@users.noreply.github.com>
2023-02-27 04:01:40 +0800

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
